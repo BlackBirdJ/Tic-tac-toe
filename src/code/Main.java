1be8da5b595f5c1f26dcb1180ce5cbc49f06a876
kreslojerry <52152485+kreslojerry@users.noreply.github.com>
2021-03-19 23:16:20 +0500

    .

--- src/code/Main.java
+++ src/code/Main.java
@@ -4,7 +4,7 @@ import code.entity.Field;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Field field = new Field();
         field.setVisible(true);
     }
